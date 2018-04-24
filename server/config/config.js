880987be7c0ea4b6304439c3ed9b6f0af4197e5a
Cesar Lachira <lachiracesar@gmail.com>
2018-04-24 01:13:24 -0500

    Seguridad

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:colan2018@ds157089.mlab.com:57089/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
